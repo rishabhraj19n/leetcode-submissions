780c73f73bea2c7717ffeb00380dcd5ebd6357b2
rishabhraj19n <rishabhraj19nov@gmail.com>
2025-08-03 18:19:24 +0000

    Sync LeetCode submission - 3Sum (cpp)

---
+++ problems/3sum/solution.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::vector<std::vector<int>> result;
+        if (nums.size() < 3) {
+            return result;
+        }
+
+        std::sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            // Skip duplicate for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    // Skip duplicates for the second element
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // Skip duplicates for the third element
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else { // sum > 0
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+};
