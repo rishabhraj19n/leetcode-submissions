a6c14a7eeba5c3c9b18540e4aac774856349f3dc
rishabhraj19n <rishabhraj19nov@gmail.com>
2025-09-07 16:56:06 +0000

    Sync LeetCode submission - Find N Unique Integers Sum up to Zero (cpp)

---
+++ problems/find_n_unique_integers_sum_up_to_zero/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    std::vector<int> sumZero(int n) {
+        std::vector<int> result;
+
+        
+        
+        if (n % 2 == 1) {
+            result.push_back(0);
+        }
+
+       
+        for (int i = 1; i <= n / 2; ++i) {
+            result.push_back(i);
+            result.push_back(-i);
+        }
+
+        return result;
+    }
+};
