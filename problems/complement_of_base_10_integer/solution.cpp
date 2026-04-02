3bdfde53328bcfecf877183ac64ea8c2d9dfdd77
rishabhraj19n <rishabhraj19nov@gmail.com>
2026-04-02 20:19:54 +0000

    Sync LeetCode submission - Complement of Base 10 Integer (cpp)

--- problems/complement_of_base_10_integer/solution.cpp
+++ problems/complement_of_base_10_integer/solution.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        int m=n;
-        int mask =0;
+        if(n==0)
+        return 1;
+        int rem , ans = 0 , mul = 1;
 
-        if ( n==0) 
-          return 1;
-        while(m!=0){
-            mask = (mask << 1 ) | 1 ;
-            m = m >> 1;
-        }       
+        while(n){
+            rem = n%2;
+            rem = rem^1;
+            n = n/2;
+            ans = ans + rem*mul;
 
-        int ans = (~n) & mask;
+            mul = mul *2;
+
+
+        }
         return ans;
-        
+
         
     }
 };
