a6171f667581eef077b758b73c2640abd30b9932
rishabhraj19n <rishabhraj19nov@gmail.com>
2026-04-13 19:13:25 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- problems/missing_number/solution.cpp
+++ problems/missing_number/solution.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        // Calculate the expected sum of numbers from 0 to n
+       
         long long expectedSum = (long long)n * (n + 1) / 2;
         
-        // Calculate the actual sum of numbers in the array
+        
         long long actualSum = 0
         ;
         for (int num : nums) {
             actualSum += num;
         }
         
-        // The missing number is the difference between the expected and actual sum
+       
         return expectedSum - actualSum;
     }
 };
