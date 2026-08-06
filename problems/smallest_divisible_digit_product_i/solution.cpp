fb278ccbabade63948eb4eb06cb0f8ae510d49b3
rishabhraj19n <rishabhraj19nov@gmail.com>
2026-08-06 09:03:00 +0000

    Sync LeetCode submission - Smallest Divisible Digit Product I (cpp)

---
+++ problems/smallest_divisible_digit_product_i/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        
+        int number = n;
+        while(true){
+            int product = 1;
+            n = number;
+            while(n){
+                product *= n%10;
+                n = n/10;
+
+            }
+
+            if(product % t == 0) break;
+            else ++number;
+
+        }
+        return number;
+
+    }
+};
